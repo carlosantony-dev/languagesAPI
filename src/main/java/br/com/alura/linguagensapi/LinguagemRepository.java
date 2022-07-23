75388f1fbdd6dedef3a6d0a2b57f248333373fd2
Carlos <carlosblecha18@gmail.com>
2022-07-23 18:59:03 -0300

    first-commit

---
+++ src/main/java/br/com/alura/linguagensapi/LinguagemRepository.java
@@ -0,0 +1,6 @@
+package br.com.alura.linguagensapi;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface LinguagemRepository extends MongoRepository<Linguagem, String> {
+}
